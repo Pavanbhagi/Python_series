f3b702247e4eb12a094206fe93d2671c242bb519
Pavanbhagi <dattapavan24@gmail.com>
2026-05-11 00:00:11 +0530

    34  New update

--- sw.js
+++ sw.js
@@ -1,16 +1,31 @@
-const CACHE = 'code-swiftly-v28';
+// Change 'v1' to 'v2' to force an update
+const CACHE_NAME = 'code-swiftly-v2'; 
 const ASSETS = [
     './',
     './index.html',
     './manifest.json',
-    'https://cdn.jsdelivr.net/pyodide/v0.23.4/full/pyodide.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js'
+    // Add any local CSS or JS files here
 ];
 
-self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
 });
 
-self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+// This part is CRITICAL: It deletes the old 'v1' cache
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((res) => res || fetch(e.request))
+    );
 });
